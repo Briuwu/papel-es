b35bbab24dafee336a137c8ac2b1972192b2eaf9
Briuwu <millontebrian@gmail.com>
2024-01-02 12:55:35 +0800

    Refactor loading component styles

--- app/loading.tsx
+++ app/loading.tsx
@@ -5,7 +5,7 @@ export default function Loading() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <div className="flex flex-col items-center justify-center">
-        <div className="flex space-x-2 justify-center items-center bg-white h-screen dark:invert">
+        <div className="flex space-x-2 justify-center items-center bg-white dark:invert">
           <span className="sr-only">Loading...</span>
           <div className="h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.3s]"></div>
           <div className="h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.15s]"></div>
