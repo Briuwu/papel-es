fefca6dbfae0bc64f9bd34fa5c95a8b1f73a461b
Briuwu <millontebrian@gmail.com>
2024-01-03 17:33:34 +0800

    Refactor handleClearanceForm function to only accept the "purpose" field

--- app/(dashboard)/request/action.ts
+++ app/(dashboard)/request/action.ts
@@ -7,10 +7,40 @@ import * as z from "zod";
 
 type FormSchema = z.infer<typeof formSchema>;
 
-export async function handleClearanceForm(data: FormSchema) {
+export async function handleClearanceForm(data: Pick<FormSchema, "purpose">) {
   const supabase = await createSupabaseServerClient();
 
   const {
     data: { session },
   } = await readUserSession();
+
+  if (!session) {
+    throw new Error("Unauthorized");
+  }
+
+  const { data: document, error: documentError } = await supabase
+    .from("documents")
+    .insert({
+      profile_id: session.user.id,
+      document_type: "barangay_clearance",
+    })
+    .select()
+    .single();
+
+  if (documentError) return JSON.stringify({ error: documentError.message });
+
+  const { error: documentClearanceError } = await supabase
+    .from("barangay_clearance_request")
+    .insert({
+      document_id: document.id,
+      profile_id: session.user.id,
+      purpose: data.purpose,
+    })
+    .select()
+    .single();
+
+  if (documentClearanceError)
+    return JSON.stringify({ error: documentClearanceError.message });
+
+  return JSON.stringify({ success: true });
 }
