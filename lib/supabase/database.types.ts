2781b5791e50a003b9a21c576fde8747751001a3
Briuwu <millontebrian@gmail.com>
2024-01-03 17:33:28 +0800

    Update database types for nullable fields

--- lib/supabase/database.types.ts
+++ lib/supabase/database.types.ts
@@ -11,38 +11,254 @@ export interface Database {
     Tables: {
       address: {
         Row: {
-          barangay: string
-          city: string
+          barangay: string | null
+          city: string | null
           created_at: string
           id: string
           profile_id: string
-          province: string
-          street: string
-          subdivision: string
+          province: string | null
+          street: string | null
+          subdivision: string | null
         }
         Insert: {
-          barangay?: string
-          city?: string
+          barangay?: string | null
+          city?: string | null
           created_at?: string
           id?: string
           profile_id: string
-          province?: string
-          street: string
-          subdivision: string
+          province?: string | null
+          street?: string | null
+          subdivision?: string | null
         }
         Update: {
-          barangay?: string
-          city?: string
+          barangay?: string | null
+          city?: string | null
           created_at?: string
           id?: string
           profile_id?: string
-          province?: string
-          street?: string
-          subdivision?: string
+          province?: string | null
+          street?: string | null
+          subdivision?: string | null
         }
         Relationships: [
           {
             foreignKeyName: "address_profile_id_fkey"
+            columns: ["profile_id"]
+            isOneToOne: false
+            referencedRelation: "profiles"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      barangay_clearance_request: {
+        Row: {
+          created_at: string
+          document_id: string | null
+          id: string
+          profile_id: string | null
+          purpose: string | null
+        }
+        Insert: {
+          created_at?: string
+          document_id?: string | null
+          id?: string
+          profile_id?: string | null
+          purpose?: string | null
+        }
+        Update: {
+          created_at?: string
+          document_id?: string | null
+          id?: string
+          profile_id?: string | null
+          purpose?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "barangay_clearance_request_document_id_fkey"
+            columns: ["document_id"]
+            isOneToOne: false
+            referencedRelation: "documents"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "barangay_clearance_request_profile_id_fkey"
+            columns: ["profile_id"]
+            isOneToOne: false
+            referencedRelation: "profiles"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      barangay_id_request: {
+        Row: {
+          created_at: string
+          document_id: string | null
+          emergency_id: string | null
+          id: string
+          profile_id: string | null
+          proof_id: string | null
+        }
+        Insert: {
+          created_at?: string
+          document_id?: string | null
+          emergency_id?: string | null
+          id?: string
+          profile_id?: string | null
+          proof_id?: string | null
+        }
+        Update: {
+          created_at?: string
+          document_id?: string | null
+          emergency_id?: string | null
+          id?: string
+          profile_id?: string | null
+          proof_id?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "barangay_id_request_document_id_fkey"
+            columns: ["document_id"]
+            isOneToOne: false
+            referencedRelation: "documents"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "barangay_id_request_emergency_id_fkey"
+            columns: ["emergency_id"]
+            isOneToOne: false
+            referencedRelation: "emergency_contact"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "barangay_id_request_profile_id_fkey"
+            columns: ["profile_id"]
+            isOneToOne: false
+            referencedRelation: "profiles"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      documents: {
+        Row: {
+          created_at: string
+          document_type: Database["public"]["Enums"]["document_types"] | null
+          id: string
+          profile_id: string | null
+          status: Database["public"]["Enums"]["document_status"] | null
+        }
+        Insert: {
+          created_at?: string
+          document_type?: Database["public"]["Enums"]["document_types"] | null
+          id?: string
+          profile_id?: string | null
+          status?: Database["public"]["Enums"]["document_status"] | null
+        }
+        Update: {
+          created_at?: string
+          document_type?: Database["public"]["Enums"]["document_types"] | null
+          id?: string
+          profile_id?: string | null
+          status?: Database["public"]["Enums"]["document_status"] | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "documents_profile_id_fkey"
+            columns: ["profile_id"]
+            isOneToOne: false
+            referencedRelation: "profiles"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      emergency_contact: {
+        Row: {
+          address_id: string | null
+          created_at: string
+          first_name: string | null
+          id: string
+          last_name: string | null
+          middle_name: string | null
+          phone_number: string | null
+          profile_id: string | null
+        }
+        Insert: {
+          address_id?: string | null
+          created_at?: string
+          first_name?: string | null
+          id?: string
+          last_name?: string | null
+          middle_name?: string | null
+          phone_number?: string | null
+          profile_id?: string | null
+        }
+        Update: {
+          address_id?: string | null
+          created_at?: string
+          first_name?: string | null
+          id?: string
+          last_name?: string | null
+          middle_name?: string | null
+          phone_number?: string | null
+          profile_id?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "emergency_contact_address_id_fkey"
+            columns: ["address_id"]
+            isOneToOne: false
+            referencedRelation: "address"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "emergency_contact_profile_id_fkey"
+            columns: ["profile_id"]
+            isOneToOne: false
+            referencedRelation: "profiles"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      incident_report_request: {
+        Row: {
+          created_at: string
+          document_id: string | null
+          id: string
+          incident_location: string | null
+          incident_narrative: string | null
+          incident_type: string | null
+          involved_parties: string[] | null
+          profile_id: string | null
+        }
+        Insert: {
+          created_at?: string
+          document_id?: string | null
+          id?: string
+          incident_location?: string | null
+          incident_narrative?: string | null
+          incident_type?: string | null
+          involved_parties?: string[] | null
+          profile_id?: string | null
+        }
+        Update: {
+          created_at?: string
+          document_id?: string | null
+          id?: string
+          incident_location?: string | null
+          incident_narrative?: string | null
+          incident_type?: string | null
+          involved_parties?: string[] | null
+          profile_id?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "incident_report_request_document_id_fkey"
+            columns: ["document_id"]
+            isOneToOne: false
+            referencedRelation: "documents"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "incident_report_request_profile_id_fkey"
             columns: ["profile_id"]
             isOneToOne: false
             referencedRelation: "profiles"
@@ -112,7 +328,8 @@ export interface Database {
       [_ in never]: never
     }
     Enums: {
-      [_ in never]: never
+      document_status: "processing" | "rejected" | "ready"
+      document_types: "barangay_id" | "barangay_clearance" | "incident_report"
     }
     CompositeTypes: {
       [_ in never]: never
