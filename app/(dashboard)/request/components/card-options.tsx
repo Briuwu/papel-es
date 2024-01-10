dca74550dbea6a0965ccb8397a44ef8601355cc0
Briuwu <millontebrian@gmail.com>
2024-01-10 16:38:30 +0800

    Fix import in CardOptions component

--- app/(dashboard)/request/components/card-options.tsx
+++ app/(dashboard)/request/components/card-options.tsx
@@ -2,7 +2,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import { useTransition } from "react";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 type CardOptionsProps = {
   id: number;
@@ -14,30 +14,35 @@ type CardOptionsProps = {
 
 export function CardOptions({ requests }: { requests: CardOptionsProps[] }) {
   const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+
+  const handleClick = (path: string) => {
+    startTransition(() => {
+      router.push(`request?opt=${path}`);
+    });
+  };
 
   return (
     <>
       {requests.map((request) => (
         <Card
           className={cn(
-            "relative z-10 flex flex-col items-center justify-between",
+            "relative z-10",
             request.active && "bg-gray-100 outline outline-blue-400",
             isPending && "pointer-events-none opacity-50",
           )}
           key={request.id}
         >
-          <CardHeader className="text-center">
-            <CardTitle>{request.name}</CardTitle>
-          </CardHeader>
-          <CardContent>{request.icon}</CardContent>
-          <Link
+          <button
             type="button"
-            className="absolute inset-0 z-20"
-            href={`?opt=${request.path}`}
-            disabled={isPending}
+            onClick={() => handleClick(request.path)}
+            className="flex h-full w-full flex-col items-center justify-between"
           >
-            <span className="sr-only">{request.name}</span>
-          </Link>
+            <CardHeader className="text-center">
+              <CardTitle>{request.name}</CardTitle>
+            </CardHeader>
+            <CardContent>{request.icon}</CardContent>
+          </button>
         </Card>
       ))}
     </>
