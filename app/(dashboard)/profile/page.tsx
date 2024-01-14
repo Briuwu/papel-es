c5292e0c0c13f146c0adee3d0c52456ca65feb36
Briuwu <millontebrian@gmail.com>
2024-01-14 15:29:15 +0800

    Add Suspense to EditProfile component

--- app/(dashboard)/profile/page.tsx
+++ app/(dashboard)/profile/page.tsx
@@ -1,13 +1,16 @@
 import { User } from "./components/user";
 import { Separator } from "@/components/ui/separator";
 import { EditProfile } from "@/app/(dashboard)/profile/components/edit-profile";
+import { Suspense } from "react";
 
 export default async function DashboardPage() {
   return (
     <>
       <div className="flex flex-col items-start justify-between gap-5 md:flex-row md:items-center">
         <User />
-        <EditProfile />
+        <Suspense fallback={null}>
+          <EditProfile />
+        </Suspense>
       </div>
       <Separator className="mt-5 h-[2px] bg-black" />
     </>
