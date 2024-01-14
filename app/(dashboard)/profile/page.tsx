56d392238e69715047f6bd244ea9211a0a7486af
Briuwu <millontebrian@gmail.com>
2024-01-14 15:34:10 +0800

    Add Skeleton component to DashboardPage and update loading message

--- app/(dashboard)/profile/page.tsx
+++ app/(dashboard)/profile/page.tsx
@@ -2,13 +2,14 @@ import { User } from "./components/user";
 import { Separator } from "@/components/ui/separator";
 import { EditProfile } from "@/app/(dashboard)/profile/components/edit-profile";
 import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default async function DashboardPage() {
   return (
     <>
       <div className="flex flex-col items-start justify-between gap-5 md:flex-row md:items-center">
         <User />
-        <Suspense fallback={null}>
+        <Suspense fallback={<Skeleton className="h-9 w-36 rounded" />}>
           <EditProfile />
         </Suspense>
       </div>
--- app/(dashboard)/request/loading.tsx
+++ app/(dashboard)/request/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className="space-y-10">
       <h1 className="text-lg font-bold uppercase sm:text-xl md:text-3xl">
-        I am requesting for ...
+        I am requesting for...
       </h1>
       <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
         <Skeleton className="h-[239px] w-full" />
